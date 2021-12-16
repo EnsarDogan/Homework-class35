5f9e7abad31eda6a1b4092d83bc4929fa7131510
ensar dogan <doganermansur@icloud.com>
2021-12-16 14:40:57 +0100

    got repository to the localhost and completed the first exeercise(ex1-giveCompliment.js)

--- 1-JavaScript/Week3/homework/ex1-giveCompliment.js
+++ 1-JavaScript/Week3/homework/ex1-giveCompliment.js
@@ -18,13 +18,27 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/1-Java
    Use `console.log` each time to display the return value of the
    `giveCompliment` function to the console.
 -----------------------------------------------------------------------------*/
-function giveCompliment(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+function giveCompliment(name) {
+  const compliment = [
+    'brilliant',
+    'cool',
+    'awesome',
+    'wonderful',
+    'terrific',
+    'perfect',
+    'great',
+    'best',
+    'super',
+    'hero'
+  ];
+  const randomNumber = Math.floor(Math.random()*10);
+  const randomCompliment = compliment[randomNumber];
+  return `You are ${randomCompliment}, ${name}!`
 }
 
 function main() {
-  // TODO substitute your own name for "HackYourFuture"
-  const myName = 'HackYourFuture';
+  
+  const myName = 'Ensar';
 
   console.log(giveCompliment(myName));
   console.log(giveCompliment(myName));
