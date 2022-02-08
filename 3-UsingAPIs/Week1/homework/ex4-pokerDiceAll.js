dd6a90900ded9b576f5514e9d7e8c9c7526ec70e
ensar dogan <doganermansur@icloud.com>
2022-02-08 20:56:52 +0100

    ex4-pokerDiceAll.js done

--- 3-UsingAPIs/Week1/homework/ex4-pokerDiceAll.js
+++ 3-UsingAPIs/Week1/homework/ex4-pokerDiceAll.js
@@ -29,7 +29,8 @@ const rollDie = require('../../helpers/pokerDiceRoller');
 function rollDice() {
   // TODO Refactor this function
   const dice = [1, 2, 3, 4, 5];
-  return rollDie(1);
+  const mappedArray = dice.map(number => {rollDie(number)})
+  return Promise.all(mappedArray)
 }
 
 function main() {
